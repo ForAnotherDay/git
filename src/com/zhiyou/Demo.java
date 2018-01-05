8dde3aca72ef8f2c644114ac3777113bf9aec48b
Pl <1554520313@qq.com>
2018-01-05 16:46:03 +0800

    第一次版本---新建项目

---
+++ src/com/zhiyou/Demo.java
@@ -0,0 +1,17 @@
+package com.zhiyou;
+
+/**  
+* @ClassName: Demo  
+* @Description: TODO
+* @author admin  
+* @date 2018年1月5日  下午4:41:45
+*  
+*/
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("第一个Git");
+	}
+	
+	
+}
