d9e6f94fd8b540a5539c9d34f83d8c20e6018797
Pl <1554520313@qq.com>
2018-01-05 16:48:33 +0800

    第一次修改

--- src/com/zhiyou/Demo.java
+++ src/com/zhiyou/Demo.java
@@ -11,6 +11,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("第一个Git");
+		System.out.println("第一次测试");
 	}
 	
 	
---
+++ src/com/zhiyou/Demo02.java
@@ -0,0 +1,16 @@
+package com.zhiyou;
+
+/**  
+* @ClassName: Demo02  
+* @Description: TODO
+* @author admin  
+* @date 2018年1月5日  下午4:47:41
+*  
+*/
+public class Demo02 {
+
+	public static void main(String[] args) {
+		System.out.println("新增文件");
+	
+	}
+}
